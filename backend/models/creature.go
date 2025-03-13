bc4aec941afdca4a6fb6e70101eee45eabaf6890
sergot <filip.sergot@gmail.com>
2025-03-13 10:17:11 +0100

    prepare repo for the actual project

--- backend/models/creature.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Creature represents a creature from Tibia
-type Creature struct {
-	Endpoint   string `json:"endpoint" bson:"endpoint"`
-	PluralName string `json:"plural_name" bson:"plural_name"`
-	Name       string `json:"name" bson:"name"`
-}
