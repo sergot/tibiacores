b0a5af337f8d6606a4230dacdeceed29346071fe
sergot <filip.sergot@gmail.com>
2026-03-24 15:38:10 +0100

    fix: replace replaceAll with replace for creature name formatting in getSoulCoreImageUrl function

--- frontend/src/utils/soulcore.ts
+++ frontend/src/utils/soulcore.ts
@@ -1,4 +1,4 @@
 export function getSoulCoreImageUrl(creatureName: string): string {
-  const filename = `${creatureName.replaceAll(' ', '_')}_Soul_Core.gif`
+  const filename = `${creatureName.replace(/ /g, '_')}_Soul_Core.gif`
   return `/assets/soulcores/${filename}`
 }
